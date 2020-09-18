1cb5fbd026fa43a329c2cc6654d7a189564b806b
KIPROTICH BRIAN <bryannkoech7@gmail.com>
2020-09-18 14:25:32 +0300

    Working on  OrderGasActivity Activity and layout

--- app/src/main/java/Alexis/m_gas/SignUpActivity.java
+++ app/src/main/java/Alexis/m_gas/SignUpActivity.java
@@ -2,8 +2,10 @@ package Alexis.m_gas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class SignUpActivity extends AppCompatActivity {
@@ -18,6 +20,15 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 onBackPressed();
+            }
+        });
+
+        Button signUp = findViewById(R.id.button_sign_up);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignUpActivity.this, OrderGasActivity.class);
+                startActivity(intent);
             }
         });
     }
